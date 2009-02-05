1cd2d2f3d0215f150d7f70d8eacc89eb229389ae
wss24@drexel.edu <wss24@drexel.edu@bb2f216a-e1d7-11dd-b00f-9fabb4b2ff13>
2009-02-05 01:54:58 +0000

    Playing with threads.

--- Main.java
+++ Main.java
@@ -4,7 +4,19 @@ public class Main
 
 	public static void main(String[] args) 
 	{
-		new MainForm().show();
+
+		//debug to test thread creation
+		//will need to do this on a gui action such
+		//as the connect command.  Connect should
+		//spawn the threads and start processing
+		//so for now, lets try to connect to Google
+		//TCPThread tcpThread = new TCPThread("64.233.161.99", 80);										//create TCP thread
+		//tcpThread.run();
+		
+        TCPThread tcpThread = new TCPThread("64.233.161.99", 80);
+        tcpThread.start();		
+		
+		new MainForm().show();			//show GUI
 	}
 
 }
--- TCPThread.java
+++ TCPThread.java
@@ -1,5 +1,56 @@
+import java.io.IOException;
+import java.net.*;
 
+//Class to represent the TCP thread for processing
+//control messages
 public class TCPThread extends Thread
 {
+	private String hostAddr = null;		//server address
+	private int hostPort = 0;			//server port num
+	private Socket sock = null;			//socket for TCP
+	
+	
+	//Constructor
+	public TCPThread(String host, int port)
+	{
+		super();						//call base constructor
+		
+		hostAddr = host;				//save host address
+		hostPort = port;				//set host port
+		
+		try 
+		{
+			//create socket and connect
+			sock = new Socket(hostAddr, hostPort);
+		} 
+		catch (Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+	}
 
+	//Entry point for the execution of the thread
+	public void run()
+	{
+		try
+		{
+			//loop forever to read from socket
+			while(true)
+			{
+				System.out.println("READ");
+				sleep(1000);
+			}
+			
+		}
+		catch(Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	public void write()
+	{
+		//write data out the socket to the server
+		System.out.println("************** WRITE");
+	}
 }
